8d55f011492d857b4607ce7eb523e5257b8e538d
zakaria <zikoxdodo@gmail.com>
2023-12-24 23:05:55 +0100

    Navbar Component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/navbar/Navbar"
+
+
+
+const App = () => {
+  return (
+    <div className="app bg-gray-20">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
