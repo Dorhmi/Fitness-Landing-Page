088a0b6efddba832e687ac931e4e85043bc5a681
zakaria <zikoxdodo@gmail.com>
2023-12-26 01:30:39 +0100

    end Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import Navbar from "@/components/navbar/Navbar"
+import { useEffect, useState } from "react"
 
 
 
 const App = () => {
+  const [selectedPage , setSelectedPage] = useState<string>('home')
+  const [isTop , setIsTop] = useState<boolean>(true)
+
+  useEffect(()=>{
+    const handleScroll = () => {
+      if(window.scrollY === 0) {
+        setIsTop(true);
+        setSelectedPage("home");
+      } 
+      if(window.scrollY !== 0) setIsTop(false);
+    }
+    window.addEventListener("scroll" , handleScroll);
+    return () => window.removeEventListener("scroll" , handleScroll);
+  }, []);
+
   return (
     <div className="app bg-gray-20">
-      <Navbar/>
+      <Navbar isTop={isTop}  selectedPage = {selectedPage} setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
